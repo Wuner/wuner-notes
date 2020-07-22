b425425f0946a9fe3624e4da1c5cd23d46e958a3
huangwanneng2 <china.heath.hwn@gmail.com>
2020-07-22 17:46:41 +0800

    create notes project

--- deploy.sh
+++ deploy.sh
@@ -25,5 +25,6 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
  git push -f 'https://github.com/HeathHwn/wuner-notes.git' master:gh-pages
+ git push -f 'https://gitee.com/HeathHwn/wuner-notes.git' master:gh-pages
 
 cd -
